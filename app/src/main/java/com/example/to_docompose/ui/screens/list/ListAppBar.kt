a98e7442bf2d613d0c2d2cc433b07978057eb425
Damian <damian.ogorek.1996@gmail.com>
2021-10-06 23:56:57 +0200

    Created view of list app bar;

---
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
@@ -0,0 +1,35 @@
+package com.example.to_docompose.ui.screens.list
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_docompose.R
+import com.example.to_docompose.ui.theme.topAppBarBackgroundColor
+import com.example.to_docompose.ui.theme.topAppBarContentColor
+
+@Composable
+fun ListAppbar() {
+    DefaultListAppBar()
+}
+
+@Composable
+fun DefaultListAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.tasks),
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
+    )
+}
+
+@Composable
+@Preview
+fun DefaultListAppBarPreview() {
+    DefaultListAppBar()
+}
