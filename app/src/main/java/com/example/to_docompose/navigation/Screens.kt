9ca02c7a40307cdafce4615c24370e0a405e7177
Damian <damian.ogorek.1996@gmail.com>
2021-10-29 23:36:37 +0200

    Created new navigation route;

--- app/src/main/java/com/example/to_docompose/navigation/Screens.kt
+++ app/src/main/java/com/example/to_docompose/navigation/Screens.kt
@@ -3,6 +3,7 @@ package com.example.to_docompose.navigation
 import androidx.navigation.NavHostController
 import com.example.to_docompose.util.Action
 import com.example.to_docompose.util.Constants.LIST_SCREEN
+import com.example.to_docompose.util.Constants.SPLASH_SCREEN
 
 class Screens(navHostController: NavHostController) {
     val list: (Action) -> Unit = { action ->
@@ -15,5 +16,13 @@ class Screens(navHostController: NavHostController) {
 
     val task: (Int) -> Unit = { taskId ->
         navHostController.navigate(route = "task/$taskId")
+    }
+
+    val splashScreen: () -> Unit = {
+        navHostController.navigate(route = "list/${Action.NO_ACTION.name}") {
+            popUpTo(SPLASH_SCREEN) {
+                inclusive = true
+            }
+        }
     }
 }
