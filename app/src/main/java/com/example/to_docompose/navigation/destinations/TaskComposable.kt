0dfe2fc4cac10771dbd0a7356d6640c00a6d7976
Damian <damian.ogorek.1996@gmail.com>
2021-10-30 14:41:39 +0200

    Updated imports; Implemented enter transition;

--- app/src/main/java/com/example/to_docompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/example/to_docompose/navigation/destinations/TaskComposable.kt
@@ -1,18 +1,23 @@
 package com.example.to_docompose.navigation.destinations
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
-import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.to_docompose.ui.screens.task.TaskScreen
 import com.example.to_docompose.ui.viewmodels.SharedViewModel
 import com.example.to_docompose.util.Action
+import com.example.to_docompose.util.Constants.NAVIGATION_ANIMATION_DURATION
 import com.example.to_docompose.util.Constants.TASK_ARGUMENT_KEY
 import com.example.to_docompose.util.Constants.TASK_SCREEN
+import com.google.accompanist.navigation.animation.composable
 
+@ExperimentalAnimationApi
 fun NavGraphBuilder.taskComposable(
     navigateToListScreen: (Action) -> Unit,
     sharedViewModel: SharedViewModel
@@ -21,7 +26,15 @@ fun NavGraphBuilder.taskComposable(
         route = TASK_SCREEN,
         arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
             type = NavType.IntType
-        })
+        }),
+        enterTransition = { _, _ ->
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(
+                    durationMillis = NAVIGATION_ANIMATION_DURATION
+                )
+            )
+        }
     ) { navBackStackEntry ->
         val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
         LaunchedEffect(key1 = taskId) {
